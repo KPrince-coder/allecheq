8bcdadf5ea014aaf903189c4915bb1ce617046bc
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-08-25 12:12:33 +0000

    Document code in AllergiesManipulation.kt file

--- app/src/main/java/dev/android/allecheq/presentation/utils/AllergiesManipulation.kt
+++ app/src/main/java/dev/android/allecheq/presentation/utils/AllergiesManipulation.kt
@@ -1,15 +1,15 @@
 package dev.android.allecheq.presentation.utils
 
-fun encodedStringToList(encodedString: String?): List<Int> {
+fun encodedStringToList(encodedString: String?): List<Int>? {
     /**
-     * @return Convert List<Int> to String representation
+     * @return Convert String representation back to List<Int>
      */
-    return encodedString?.split(",")?.map { it.toInt() } ?: emptyList()
+    return encodedString?.split(",")?.mapNotNull { it.toIntOrNull() }
 }
 
-fun listToEncodedList(list: List<Int>): String {
+fun listToEncodedString(list: List<Int>): String {
     /**
-     *@return Convert String representation back to List<Int>
+     *@return Convert List<Int> to String representation
      */
     return list.joinToString(", ")
 }
