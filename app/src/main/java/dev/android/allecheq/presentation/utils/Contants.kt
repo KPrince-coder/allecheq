377520bfd615abbd95d935bd5f66bd9def933d05
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-08-27 16:27:48 +0000

    Create a constants util file for keeping constant values

---
+++ app/src/main/java/dev/android/allecheq/presentation/utils/Contants.kt
@@ -0,0 +1,13 @@
+package dev.android.allecheq.presentation.utils
+
+// constant values used in the app
+const val VALUE_2 = 2
+const val VALUE_4 = 4
+const val VALUE_8 = 8
+const val VALUE_12 = 12
+const val VALUE_16 = 16
+const val VALUE_20 = 20
+const val VALUE_24 = 24
+const val VALUE_32 = 32
+const val VALUE_36 = 36
+const val VALUE_40 = 40
