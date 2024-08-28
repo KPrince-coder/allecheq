cd906f64c43c35b36354dcd5e085b738438b8422
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-08-28 17:04:34 +0000

    Add navigation system to MainActivity.kt file

--- app/src/main/java/dev/android/allecheq/MainActivity.kt
+++ app/src/main/java/dev/android/allecheq/MainActivity.kt
@@ -10,38 +10,33 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import dev.android.allecheq.presentation.navigations.NavigationGraph
+import dev.android.allecheq.presentation.screens.HomeScreen
+import dev.android.allecheq.presentation.screens.OnboardingScreen1
+import dev.android.allecheq.presentation.screens.OnboardingScreen2
 import dev.android.allecheq.ui.theme.AlleCheqTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // colors for status bar
+        // val backgroundLight = Color(0xFFFAF8FF).toArgb()
+        // val backgroundDark = Color(0xFF121318).toArgb()
+        enableEdgeToEdge(
+            // statusBarStyle = SystemBarStyle.auto(backgroundLight, backgroundDark),
+        )
+
         setContent {
             AlleCheqTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                //
+                //     HomeScreen(modifier = Modifier.padding(paddingValues))
+                // }
+                NavigationGraph()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlleCheqTheme {
-        Greeting("Android")
-    }
-}
