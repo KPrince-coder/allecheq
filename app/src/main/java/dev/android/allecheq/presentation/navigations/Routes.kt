df94da4c8343b7a76b91dfe006e47b8acb6b53b0
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-08-31 15:12:38 +0000

    Change all routes from type-safe

--- app/src/main/java/dev/android/allecheq/presentation/navigations/Routes.kt
+++ app/src/main/java/dev/android/allecheq/presentation/navigations/Routes.kt
@@ -1,17 +1,41 @@
 package dev.android.allecheq.presentation.navigations
 
-import kotlinx.serialization.Serializable
+import dev.android.allecheq.presentation.utils.Screen
 
 /**
  * @description
  * Different routes for the application
  */
-@Serializable
-sealed class Routes {
-    @Serializable
-    data object OnboardingScreen1:Routes()
-    @Serializable
-    data object OnboardingScreen2:Routes()
-    @Serializable
-    data object HomeScreen:Routes()
+// @Serializable
+// sealed class Routes(val screen: String) {
+//     @Serializable
+//     data object OnboardingScreen1 : Routes()
+//
+//     @Serializable
+//     data object OnboardingScreen2 : Routes()
+//
+//     @Serializable
+//     data object HomeScreen : Routes()
+//
+//     @Serializable
+//     data object ScreensWithBottomNavBar : Routes()
+// }
+object ScreensRoute {
+    internal val OnboardingScreen1 = Screen("onboarding_screen_1")
+    internal val OnboardingScreen2 = Screen("onboarding_screen_2")
+    internal val HomeScreen = Screen("home_screen")
+    internal val ScreensWithBottomNavBar = Screen("main_screen")
 }
+
+sealed class Routes(val screen: Screen) {
+    data object OnboardingScreen1 : Routes(ScreensRoute.OnboardingScreen1)
+
+    data object OnboardingScreen2 : Routes(ScreensRoute.OnboardingScreen2)
+
+    data object HomeScreen : Routes(ScreensRoute.HomeScreen)
+
+    data object ScreensWithBottomNavBar : Routes(ScreensRoute.ScreensWithBottomNavBar)
+}
+
+
+
