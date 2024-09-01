dd92b60ba3abf89cf559c8db4984c7b1ac7593a7
Prince Kyeremeh <kyeremehprince68@gmail.com>
2024-09-01 15:14:41 +0000

    Test screens

--- app/src/main/java/dev/android/allecheq/presentation/screens/ScreensWithButtomNavBar.kt
+++ app/src/main/java/dev/android/allecheq/presentation/screens/ScreensWithButtomNavBar.kt
@@ -1,16 +1,7 @@
 package dev.android.allecheq.presentation.screens
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.captionBar
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,27 +21,5 @@ fun ScreensWithBottomNavBar(
         }
     ) {
         screenContent(it)
-        // ScreensWithBottomNavGraph(navController = navController, modifier = Modifier.padding(it))
     }
 }
-// @Preview(
-//     name = "light mode",
-//     showSystemUi = true,
-//     showBackground = true,
-//     device = Devices.PIXEL_6
-// )
-// @Preview(
-//     name = "dark mode",
-//     uiMode = Configuration.UI_MODE_NIGHT_YES,
-//     showSystemUi = true,
-//     showBackground = true,
-//     device = Devices.PIXEL_6
-// )
-// @Composable
-// private fun Preview() {
-//     AlleCheqTheme {
-//         ScreensWithBottomNavBar(navController = rememberNavController(), screenContent = {
-//             ScanScreen(paddingValues = it)
-//         })
-//     }
-// }
