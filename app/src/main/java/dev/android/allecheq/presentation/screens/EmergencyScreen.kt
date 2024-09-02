ca358a2d6f47b1e0f0b60c726f57d2c373ee9e5f
Theophilus Atuah <theoatuah@gmail.com>
2024-09-02 16:29:21 +0000

    Add more editing and design to EmergencyScreen

--- app/src/main/java/dev/android/allecheq/presentation/screens/EmergencyScreen.kt
+++ app/src/main/java/dev/android/allecheq/presentation/screens/EmergencyScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
@@ -36,17 +38,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.android.allecheq.R
@@ -64,8 +70,6 @@ import dev.android.allecheq.presentation.viewmodel.EmergencyContactViewModel
 import dev.android.allecheq.presentation.viewmodel.viewmodelfactory.EmergencyContactViewModelFactory
 import dev.android.allecheq.ui.theme.FontFam
 import java.util.Locale
-
-
 
 
 @Composable
@@ -119,8 +123,14 @@ private fun EmergencyScreenContent(
                 .padding(horizontal = VALUE_16.dp, vertical = VALUE_12.dp)
         ) {
             Title()
-            Contacts(contactList = contactList, onDeleteClick = onDeleteClick)
-            AddContactButton(onClick = { showAddContactDialog = true }) // Update to handle click
+
+            if (contactList.isEmpty()) {
+                NoContact(showAddContactDialog = { showAddContactDialog = true })
+
+            }
+            Contacts(contactList = contactList, onDeleteClick = onDeleteClick, onClick = {
+                showAddContactDialog = true
+            })
         }
     }
 
@@ -163,29 +173,68 @@ private fun Title(modifier: Modifier = Modifier) {
 @Composable
 fun Contacts(
     contactList: List<EmergencyContact>,
+    onClick: () -> Unit,
     onDeleteClick: (contact: EmergencyContact) -> Unit,
     modifier: Modifier = Modifier
 ) {
-
-    LazyColumn(modifier = modifier.padding(top = VALUE_40.dp)) {
-        item {
-            Text(
-                text = stringResource(R.string.contacts),
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontSize = VALUE_20.sp,
-                    color = MaterialTheme.colorScheme.outline
+    Box(modifier = modifier) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = VALUE_40.dp)
+        ) {
+            item {
+                Text(
+                    text = stringResource(R.string.contacts),
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = VALUE_20.sp,
+                        color = MaterialTheme.colorScheme.outline
+                    )
                 )
-            )
+            }
+
+            items(contactList) { contact ->
+                Contact(
+                    contact = contact,
+                    modifier = Modifier.padding(top = VALUE_12.dp),
+                    onDeleteClick = onDeleteClick
+                )
+            }
+
+
         }
-        items(contactList) { contact ->
-            Contact(
-                contact = contact,
-                modifier = Modifier.padding(top = VALUE_12.dp),
-                onDeleteClick = onDeleteClick
-            )
-        }
+
+        AddContactButton(onClick = onClick)
+
     }
+}
+
+@Composable
+private fun NoContact(showAddContactDialog: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = stringResource(R.string.no_contacts_available),
+            style = TextStyle(
+                fontSize = VALUE_20.sp,
+                fontWeight = FontWeight.W500,
+                fontFamily = FontFam.Inter.fontFamily,
+                color = MaterialTheme.colorScheme.primary
+            ),
+            modifier = Modifier.wrapContentSize(Alignment.Center)
+        )
+
+        AddContactButton(
+            onClick = showAddContactDialog,
+        )
+    }
+
 }
 
 
@@ -214,8 +263,9 @@ private fun Contact(
                 text = initials,
                 color = MaterialTheme.colorScheme.onPrimaryContainer,
                 style = TextStyle(
-                    fontSize = VALUE_16.sp,
-                    fontWeight = FontWeight.Bold
+                    fontSize = VALUE_20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFam.Inter.fontFamily
                 )
             )
         }
@@ -224,15 +274,16 @@ private fun Contact(
             Text(
                 text = "${
                     contact.name.replaceFirstChar {
-                        if (it.isLowerCase()) it
-                            .titlecase(Locale.getDefault())
+                        if (it.isLowerCase())
+                            it.titlecase(Locale.getDefault())
                         else it.toString()
                     }
                 } (${contact.relationship.lowercase()})",
                 style = TextStyle(
                     fontSize = VALUE_16.sp,
                     fontWeight = FontWeight.W600,
-                    color = MaterialTheme.colorScheme.outline
+                    color = MaterialTheme.colorScheme.outline,
+                    fontFamily = FontFam.Inter.fontFamily
                 )
             )
             Spacer(modifier = Modifier.height(VALUE_4.dp))
@@ -241,7 +292,9 @@ private fun Contact(
                 style = TextStyle(
                     fontSize = VALUE_12.sp,
                     fontWeight = FontWeight.W400,
-                    color = MaterialTheme.colorScheme.outline
+                    color = MaterialTheme.colorScheme.outline,
+                    fontFamily = FontFam.Inter.fontFamily
+
                 )
             )
         }
@@ -312,16 +365,40 @@ fun ConfirmDeleteDialog(
         onDismissRequest = onDismiss,
         confirmButton = {
             Button(onClick = onConfirm) {
-                Text("Delete")
+                Text(
+                    text = stringResource(R.string.delete),
+                    style = TextStyle(
+                        fontFamily = FontFam.Inter.fontFamily
+                    )
+                )
             }
         },
         dismissButton = {
             Button(onClick = onDismiss) {
-                Text("Cancel")
+                Text(
+                    text = stringResource(R.string.cancel),
+                    style = TextStyle(
+                        fontFamily = FontFam.Inter.fontFamily
+                    )
+                )
             }
         },
-        title = { Text("Delete Contact") },
-        text = { Text("Are you sure you want to delete this contact?") }
+        title = {
+            Text(
+                text = stringResource(R.string.delete_contact),
+                style = TextStyle(
+                    fontFamily = FontFam.Inter.fontFamily
+                )
+            )
+        },
+        text = {
+            Text(
+                text = stringResource(R.string.delete_contact_text),
+                style = TextStyle(
+                    fontFamily = FontFam.Inter.fontFamily
+                )
+            )
+        }
     )
 }
 
@@ -334,6 +411,13 @@ fun AddContactDialog(
     var name by remember { mutableStateOf("") }
     var relationship by remember { mutableStateOf("") }
     var phoneNumber by remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
+    val (
+        focusRequester1,
+        focusRequester2,
+        focusRequester3
+    ) = createRefs()
 
     AlertDialog(
         onDismissRequest = onDismiss,
@@ -349,35 +433,107 @@ fun AddContactDialog(
                     )
                 }
             ) {
-                Text("Add")
+                Text(
+                    text = stringResource(R.string.add),
+                    style = TextStyle(
+                        fontFamily = FontFam.Inter.fontFamily,
+                        fontWeight = FontWeight.W600
+                    )
+                )
             }
         },
         dismissButton = {
             Button(onClick = onDismiss) {
-                Text("Cancel")
+                Text(
+                    text = stringResource(id = R.string.cancel),
+                    style = TextStyle(
+                        fontFamily = FontFam.Inter.fontFamily,
+                        fontWeight = FontWeight.W600
+                    )
+                )
             }
         },
-        title = { Text("Add Contact") },
+        title = {
+            Text(
+                text = stringResource(R.string.add_contact),
+                style = TextStyle(
+                    fontFamily = FontFam.Inter.fontFamily,
+                    fontWeight = FontWeight.W600,
+                    fontSize = VALUE_20.sp
+                )
+            )
+        },
         text = {
             Column {
                 OutlinedTextField(
                     value = name,
                     onValueChange = { name = it },
-                    label = { Text("Name") }
+                    label = { Text(stringResource(R.string.name)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    ),
+                    modifier = Modifier
+                        .focusRequester(focusRequester1)
                 )
+
+
                 OutlinedTextField(
                     value = relationship,
                     onValueChange = { relationship = it },
-                    label = { Text("Relationship") }
+                    label = {
+                        Text(
+                            stringResource(R.string.relationship),
+                            fontFamily = FontFam.Inter.fontFamily
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    ),
+                    modifier = Modifier
+                        .focusRequester(focusRequester2)
                 )
+
+
                 OutlinedTextField(
                     value = phoneNumber,
                     onValueChange = { phoneNumber = it },
-                    label = { Text("Phone Number") }
+                    label = {
+                        Text(
+                            stringResource(R.string.phone_number),
+                            fontFamily = FontFam.Inter.fontFamily
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            onAddContact(
+                                EmergencyContact(
+                                    name = name,
+                                    relationship = relationship,
+                                    phoneNumber = phoneNumber
+                                )
+                            )
+
+                        }
+                    ),
+                    modifier = Modifier
+                        .focusRequester(focusRequester3)
                 )
             }
         }
     )
 }
-
 
